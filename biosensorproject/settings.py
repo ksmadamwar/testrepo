1e2e8eddb9cf91b941ae8bfc3ed43e08deed6042
Komal Madamwar <ksmadamwar@gmail.com>
2022-05-26 17:26:58 -0400

    changed django settings

--- biosensorproject/settings.py
+++ biosensorproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ek0x(vk3)tr^$jiwo5_ffqy-c-ie_2-sg*rt8kt5&oe*u)#t_6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
